6064de6ab7d640a2a556ccd82b515145fe5136c0
WaylonYuen <1664940243lwy@gmail.com>
2018-10-01 15:01:20 +0800

    changes: #01 heading

--- C Exercise/#01_helloWord.c
+++ C Exercise/#01_helloWord.c
@@ -1,3 +1,10 @@
+//
+//  #01_helloWold.c
+//  My First C program
+//
+//  Created by Waylon.Yuen on 1/10/2018.
+//  Copyright © 2018年 Oracragron. All rights reserved.
+//
 #include<stdio.h>
 
 int main(void){
