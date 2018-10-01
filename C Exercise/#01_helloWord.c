f099e0148550a7709020c56aa53dbe04157a2d2b
WaylonYuen <1664940243lwy@gmail.com>
2018-10-01 15:11:30 +0800

    update #01

--- C Exercise/#01_helloWord.c
+++ C Exercise/#01_helloWord.c
@@ -5,11 +5,11 @@
 //  Created by Waylon.Yuen on 1/10/2018.
 //  Copyright © 2018年 Oracragron. All rights reserved.
 //
-#include<stdio.h>
+#include<stdio.h>               // Lirary   標準輸入輸出頭文件
 
-int main(void){
+int main(void){                 //主程式
 
-    printf("Hello world! \n");
+    printf("Hello world! \n");  //打印“Hello world！”輸出信息
 
-    return 0;
+    return 0;                   //整數型主程式 返回數值‘0’
 }
