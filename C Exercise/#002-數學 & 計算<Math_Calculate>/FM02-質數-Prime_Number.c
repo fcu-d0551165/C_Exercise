aeec61c107407637a154b4418103ffbaf2b011b0
WaylonYuen <1664940243lwy@gmail.com>
2018-10-18 15:25:06 +0800

    update

---
+++ C Exercise/#002-數學 & 計算<Math_Calculate>/FM02-質數-Prime_Number.c
@@ -0,0 +1,16 @@
+//#FM02-質數<Prime_Number>.c
+#include<stdio.h>
+
+int main(int argc, char const *argv[]){
+    
+    int i;
+    int number;
+
+    scanf("%d", &number);
+
+    for(i=1; i<=number/2; i++){
+
+    }
+
+    return 0;
+}
