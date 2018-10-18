e0288e72b1fd91ce1d7845738d96167ada744a30
WaylonYuen <1664940243lwy@gmail.com>
2018-10-18 15:50:34 +0800

    Update C Exercise/#002-數學 & 計算<Math_Calculate>/FM02-質數-Prime_Number.c

--- C Exercise/#002-數學 & 計算<Math_Calculate>/FM02-質數-Prime_Number.c
+++ C Exercise/#002-數學 & 計算<Math_Calculate>/FM02-質數-Prime_Number.c
@@ -4,13 +4,25 @@
 int main(int argc, char const *argv[]){
     
     int i;
+    int flag = 0;
     int number;
 
     scanf("%d", &number);
 
-    for(i=1; i<=number/2; i++){
-
+    for(i=2; i<number; i++){
+        if(number%i == 0){  //能被整除
+            flag = 1;
+            break;
+        }
     }
+    /*不能夠被整除*/
+    if(flag == 0){
+        printf("%d is a Prime number! \n", number);
+    }
+    else{
+        printf("%d is not a Prime number! \n", number);
+    }
+
 
     return 0;
 }
